8bdc865801658efb6f3ccfbb1bcb6c2f55963b0a
Andrii <itehnolog@ukr.net>
2023-07-10 12:34:08 +0300

    HW3 added

---
+++ passwd_class.py
@@ -0,0 +1,58 @@
+import random
+import string
+
+
+class PasswordGenerator:
+
+    def __init__(self) -> None:
+        self.length = 8
+        self.include_uppercase = True
+        self.include_lowercase = True
+        self.include_digits = True
+        self.include_special_chars = True
+
+    def ramdom_character(self):
+        # method wich generate random character
+        random.seed()
+        random_list = []
+        if self.include_digits:
+            random_list.append(random.choice(string.digits))
+        if self.include_lowercase:
+            random_list.append(random.choice(string.ascii_lowercase))
+        if self.include_uppercase:
+            random_list.append(random.choice(string.ascii_uppercase))
+        if self.include_special_chars:
+            random_list.append(random.choice(string.punctuation))
+        try:
+            return random.choice(random_list)
+        except:
+            print("Wrong conditions!!! Please run csript one more time.")
+            exit(0)
+
+    def generate_password(self):
+        # method wich generate random password
+        password = ''
+        # password = password + \
+        #     random.choice(string.ascii_lowercase) + \
+        #     random.choice(string.ascii_uppercase) + \
+        #     random.choice(string.digits) + random.choice(string.punctuation)
+        for i in range(self.length):
+            password = password + self.ramdom_character()
+
+        return f"Yuor password is: {password}"
+
+    def check_password(self, pasword):
+        pass
+
+    def change_length(self):
+        try:
+            self.length = int(input("Enter your length(min 8, max 256): "))
+        except ValueError:
+            print("Length must be only number!")
+        self.check_password_length()
+        return f"Password length changed to {self.length}"
+
+    def check_password_length(self):
+        if self.length < 8 or self.length > 256:
+            self.length = 8
+            print("Wrong length! Must be between 8 and 256 characters!")
